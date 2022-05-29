2f4b1bbcc118b40cdc3ab9f866c8b3e344ef1214
Amne Sampaio <39910460+AmneSampaio@users.noreply.github.com>
2022-05-29 13:55:58 -0300

    Initial commit - Spring structure

---
+++ src/main/java/com/magic/cardsInventory/CardsInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.magic.cardsInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magic/cardsInventory/CardsInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magic.cardsInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
